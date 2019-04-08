ef1fbc9ae20b02fa4f9225065fdbbebc607593de
mzydron <zydronmaciej@gmail.com>
2019-04-08 20:27:01 +0200

    Prepared function to save html to file for easier search of interesting values

--- GPUPriceScrapper.py
+++ GPUPriceScrapper.py
@@ -14,8 +14,15 @@ class WebScrapper:
 
         return self.soup.find_all(atrrs={'class': 'price'})
 
+    def saveHttml(self):
+
+        with open("html.txt", 'w') as htmlfile:
+            htmlfile.write(self.soup.text)
+
+
 
 page = 'https://www.ceneo.pl/;szukaj-geforce+rtx+2070'
 
 ws = WebScrapper(page)
+ws.saveHttml()
 print(ws.getPrice())
