a2d7a32a719e0ea6aae08bd46b5cd34157930a1e
mzydron <zydronmaciej@gmail.com>
2019-04-13 12:11:47 +0200

    Trying to find proper parameter

--- GPUPriceScrapper.py
+++ GPUPriceScrapper.py
@@ -9,20 +9,22 @@ class WebScrapper:
         webpage = requests.get(url)
         self.soup = BeautifulSoup(webpage.text, 'html.parser')
 
-
     def getPrice(self):
 
-        return self.soup.find_all(atrrs={'class': 'price'})
+        divs = self.soup.find_all("div", {"class": "cat-prod-row-content"})
+        gpuDivs = self.soup.find_all('a', {"class": "js_seoUrl js_clickHash go-to-product"})
 
-    def saveHttml(self):
+        return gpuDivs
+
+    def saveHtml(self):
 
         with open("html.txt", 'w') as htmlfile:
             htmlfile.write(self.soup.text)
 
 
-
-page = 'https://www.ceneo.pl/;szukaj-geforce+rtx+2070'
+page = 'https://www.ceneo.pl/;szukaj-geforce+rtx+2070+oc'
 
 ws = WebScrapper(page)
-ws.saveHttml()
+# ws.saveHtml()
 print(ws.getPrice())
+
